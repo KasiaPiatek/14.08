493a2cbe61d5e791f8e952636d32b442d5243e41
Kasia Piatek <piatekkatarzynaolga@gmail.com>
2018-03-10 11:34:29 +0100

    add components

---
+++ components/App.js
@@ -0,0 +1,43 @@
+App = React.createClass({
+    render: function() {
+
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+
+    handleSearch: function(searchingText) {  // 1.
+    this.setState({
+      loading: true  // 2.
+    });
+    this.getGif(searchingText, function(gif) {  // 3.
+      this.setState({  // 4
+        loading: false,  // a
+        gif: gif,  // b
+        searchingText: searchingText  // c
+      });
+    }.bi
+
+        return (
+          <div style={styles}>
+                <h1>Wyszukiwarka GIFow!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search />
+            <Gif 
+                loading={this.state.loading}
+                url={this.state.gif.url}
+                sourceUrl={this.state.gif.sourceUrl}
+            />
+          </div>
+        );
+    }
+});
